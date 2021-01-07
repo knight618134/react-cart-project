d79f1b58c815443818f33eac37b134d8cae1aa2b
tsaistorm <jason@staging.com.tw>
2021-01-08 01:50:48 +0800

    feat : react store first push

---
+++ src/component/Login.js
@@ -0,0 +1,27 @@
+import React from 'react'
+class Login extends React.Component {
+  render() {
+    return (
+      <div className='login-wrapper'>
+        <form className='box login-box'>
+          <div className='field'>
+            <label className='label'>Email</label>
+            <div className='control'>
+              <input type='text' placeholder='Email' name='email' />
+            </div>
+          </div>
+          <div className='field'>
+            <label className='label'>Password</label>
+            <div className='control'>
+              <input type='password' placeholder='Password' name='password' />
+            </div>
+          </div>
+          <div className='control'>
+            <button className='button is-fullwidth is-primary'>Login</button>
+          </div>
+        </form>
+      </div>
+    )
+  }
+}
+export default Login
